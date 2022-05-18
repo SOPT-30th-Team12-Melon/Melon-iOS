f42f2aa4faeb4badfc5dc3ad1b966ee16e87f979
Yujin Lee <yj587878@gmail.com>
2022-05-18 18:56:54 +0900

    [ADD] Extension 파일들 추가 및 info.plist 설정

---
+++ Melon-iOS/Global/Extension/UIColor+.swift
@@ -0,0 +1,52 @@
+//
+//  UIColor+.swift
+//  Melon-iOS
+//
+//  Created by 이유진 on 2022/05/17.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var green2: UIColor {
+        return UIColor(red: 96.0 / 255.0, green: 206.0 / 255.0, blue: 77.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var green1: UIColor {
+        return UIColor(red: 105.0 / 255.0, green: 228.0 / 255.0, blue: 73.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var white: UIColor {
+        return UIColor(white: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var lightGray: UIColor {
+        return UIColor(white: 208.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray: UIColor {
+        return UIColor(white: 160.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var point1: UIColor {
+        return UIColor(red: 99.0 / 255.0, green: 49.0 / 255.0, blue: 66.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var point2: UIColor {
+        return UIColor(red: 99.0 / 255.0, green: 48.0 / 255.0, blue: 43.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var point3: UIColor {
+        return UIColor(red: 42.0 / 255.0, green: 77.0 / 255.0, blue: 104.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var darkGray: UIColor {
+        return UIColor(white: 18.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var darkGray2: UIColor {
+        return UIColor(white: 48.0 / 255.0, alpha: 1.0)
+    }
+    
+}
