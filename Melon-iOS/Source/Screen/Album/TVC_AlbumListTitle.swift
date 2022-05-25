a1127958a0b575c9a2bf3148ad102c6e8d90be14
lee-euijin <leeuj9663@naver.com>
2022-05-25 10:10:54 +0900

    [MOD] 상세 페이지 Cell 3 - 수록곡 목록 Header (#3)

---
+++ Melon-iOS/Source/Screen/Album/TVC_AlbumListTitle.swift
@@ -0,0 +1,23 @@
+//
+//  TVC_AlbumList_title.swift
+//  Melon-iOS
+//
+//  Created by 이의진 on 2022/05/25.
+//
+
+import UIKit
+
+class TVC_AlbumList_title: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
