4408f984d4810423e04fa1c0f088eae12b628927
lee-euijin <leeuj9663@naver.com>
2022-05-27 13:39:37 +0900

    [MOD] MusicProject, New Music 글씨체 수정, 셀 제작 및 연결 완료 (#8)

---
+++ Melon-iOS/Source/Screen/Main/NewMusic/NewMusicItem/NewMusicItemCVC.swift
@@ -0,0 +1,17 @@
+//
+//  NewMusicItemCVC.swift
+//  Melon-iOS
+//
+//  Created by 이의진 on 2022/05/27.
+//
+
+import UIKit
+
+class NewMusicItemCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
