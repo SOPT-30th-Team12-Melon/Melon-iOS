f929da155ff31fb44e4b77bb88a6007956deedaf
Yujin Lee <yj587878@gmail.com>
2022-06-01 02:57:29 +0900

    [FEAT] 텍스트뷰 패딩 추가

--- Melon-iOS/Source/Screen/Posting/PostingVC.swift
+++ Melon-iOS/Source/Screen/Posting/PostingVC.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class PostingVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var applyButton: UIButton!
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    @IBAction func cancelButtonDidTap(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func applyButtonDidTap(_ sender: Any) {
     }
-    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setCommentTextView()
+    }
+    
+    func setCommentTextView() {
+        commentTextView.textContainerInset = UIEdgeInsets(top: 16,left: 25,bottom: 16,right: 33)
+    }
 
 }
